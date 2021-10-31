dd68bbf80ba800753f1bca99a6517810c98f854f
vyasinskiy <vyasinskiy@tradingview.com>
2021-10-31 22:02:47 +0300

    Replace browser router and auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,15 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
-import { CreatePage } from "./pages/create-page";
-import { DetailPage } from "./pages/detail-page";
-import { LinksPage } from "./pages/links-page";
-import { AuthPage } from './pages/auth-page'
+import { BrowserRouter } from 'react-router-dom';
+import { useAuthRoutes } from './pages/use-auth-routes';
 
 export function App() {
   const isAuth = false;
+  const authRoutes = useAuthRoutes(isAuth);
 
-  if (!isAuth) {
-    return (
-      <Switch>
-          <Route path="/login">
-              <AuthPage/>
-          </Route>
-          <Redirect to="/login"/>
-      </Switch>
-    ) 
-  } else {
-    return (
-      <Switch>
-          <Route path="/links" exact>
-              <LinksPage/>
-          </Route>
-          <Route path="/create" exact>
-              <CreatePage/>
-          </Route>
-          <Route path="/detail/:id">
-              <DetailPage/>
-          </Route>
-          <Redirect to="/create"/>
-      </Switch>
-    )
-  }
+  return (
+    <BrowserRouter>
+      {authRoutes}
+    </BrowserRouter>
+  )
+  
 }
